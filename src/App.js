28555d96966e670f245d398481e433c617fe21d5
Vesa Kilpiäinen <vesa.kilpiainen@qvantel.com>
2020-11-11 12:12:38 +0200

    form and state hooks for input values, useEffect to handle combining, combining algorith still missing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [combination, setCombination] = useState("asdf  qwer");
+
+  useEffect(() => {
+    const vowels = ["a", "e", "i", "o", "u", "y", "ä", "ö", "å"];
+    const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "z"];
+
+    //vokaali ennen konsonanttia, vokaalin jälkeen katko
+    //kaksi peräkkäistä vokaalia, katko toisen jälkeen
+    //kaksi perättäistä konsonanttia, katko niiden keskeltä
+    
+    setCombination(name1 + name2);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +35,9 @@ function App() {
         >
           Learn React
         </a>
+        <input type="text" value={name1} onChange={e => setName1(e.target.value)} />
+        <input type="text" value={name2} onChange={e => setName2(e.target.value)} />
+        <div>{combination}</div>
       </header>
     </div>
   );
