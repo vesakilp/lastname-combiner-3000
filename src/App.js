24068d29efd3a36cc4fa1270a83fe7b61e11735d
Vesa Kilpiäinen <vesa.kilpiainen@qvantel.com>
2020-12-14 21:31:43 +0200

    created splitting logic, fixed tests, problems with state props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
 
@@ -9,16 +9,57 @@ function App() {
   const [name2, setName2] = useState("");
   const [combination, setCombination] = useState("asdf  qwer");
 
+  const usePrevious = (value) => {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  };
+
+  const prevName1 = usePrevious(name1);
+  const prevName2 = usePrevious(name2);
+
   useEffect(() => {
+    setCombination(splitName(name1, true) + splitName(name2, false));
+  });
+
+  const substring = (string, index, returnFirstPart) => {
+    if(returnFirstPart) return string.substring(0, index);
+    else return string.substring(index, string.length);
+  };
+
+  const splitName = (name, returnFirstPart) => {
+    console.log("name", name);
     const vowels = ["a", "e", "i", "o", "u", "y", "ä", "ö", "å"];
     const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "z"];
 
-    //1. vokaali ennen konsonanttia, vokaalin jälkeen katko
-    //2. kaksi peräkkäistä vokaalia, katko toisen jälkeen
-    //3. kaksi perättäistä konsonanttia, katko niiden keskeltä
+    let splitName = "";
 
-    setCombination(name1 + name2);
-  });
+    for (var i = 0; i < name.length; i++) {
+      //1. vokaali ennen konsonanttia, konsonantin jälkeen katko
+      if(vowels.includes(name.charAt(i)) && consonants.includes(name.charAt(i+1))) {
+        console.log("1", name.charAt(i));
+        splitName = substring(name, i+2, returnFirstPart);
+        break;
+      }
+      //2. kaksi peräkkäistä vokaalia, katko toisen jälkeen, pitää testata että ei olla ekassa kirjaimessa
+      if(i < 0 && vowels.includes(name.charAt(i) && vowels.includes(name.charAt(i-1)))) {
+        console.log("2");
+        splitName = substring(name, i+1, returnFirstPart);
+        break;
+      }
+
+      //3. kaksi perättäistä konsonanttia, katko niiden keskeltä, pitää testata että ei olla ekassa kirjaimessa
+      if(1 < 0 && consonants.includes(name.charAt(i) && consonants.includes(name.charAt(i-1)))) {
+        console.log("3");
+        splitName = substring(name, i+1, returnFirstPart);
+        break;
+      }
+      console.log("splitName", splitName);
+    }
+    return splitName;
+  };
 
   return (
     <div className="App">
--- src/App.test.js
+++ src/App.test.js
@@ -19,164 +19,179 @@ test('korhonen + virtanen = kortanen', () => {
 test('mäkinen + nieminen = mäminen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "mäkinen" } });
+  expect(inputName1.value).toBe("mäkinen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "nieminen" } });
+  expect(inputName2.value).toBe("nieminen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("mäminen")).toBeInTheDocument()
 
 });
 
 test('kortanen + mäminen = korminen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "kortanen" } });
+  expect(inputName1.value).toBe("kortanen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "mäminen" } });
+  expect(inputName2.value).toBe("mäminen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("korminen")).toBeInTheDocument()
 
 });
 
-test('kortanen + mäminen = mätanen', () => {
+test('mäminen + kortanen = mätanen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "mäminen" } });
+  expect(inputName1.value).toBe("mäminen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "kortanen" } });
+  expect(inputName2.value).toBe("kortanen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("mätanen")).toBeInTheDocument()
 
 });
 
-test('korhonen + virtanen = virhonen', () => {
+test('virtanen + korhonen = virhonen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "virtanen" } });
+  expect(inputName1.value).toBe("virtanen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "korhonen" } });
+  expect(inputName2.value).toBe("korhonen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("virhonen")).toBeInTheDocument()
 
 });
 
-test('mäkinen + nieminen = niekinen', () => {
+test('nieminen + mäkinen = niekinen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "nieminen" } });
+  expect(inputName1.value).toBe("nieminen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "mäkinen" } });
+  expect(inputName2.value).toBe("mäkinen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("niekinen")).toBeInTheDocument()
 
 });
 
 test('virhonen + niekinen = virkinen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "virhonen" } });
+  expect(inputName1.value).toBe("virhonen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "niekinen" } });
+  expect(inputName2.value).toBe("niekinen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("virkinen")).toBeInTheDocument()
 
 });
 
-test('virhonen + niekinen = niehonen', () => {
+test('niekinen + virhonen = niehonen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "niekinen" } });
+  expect(inputName1.value).toBe("niekinen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "virhonen" } });
+  expect(inputName2.value).toBe("virhonen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("niehonen")).toBeInTheDocument()
 
 });
 
 test('kortanen + niekinen = korkinen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "kortanen" } });
+  expect(inputName1.value).toBe("kortanen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "niekinen" } });
+  expect(inputName2.value).toBe("niekinen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("korkinen")).toBeInTheDocument()
 
 });
 
-test('kortanen + niekinen = nietanen', () => {
+test('niekinen + kortanen = nietanen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "niekinen" } });
+  expect(inputName1.value).toBe("niekinen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "kortanen" } });
+  expect(inputName2.value).toBe("kortanen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("nietanen")).toBeInTheDocument()
 
 });
 
 test('virhonen + mäminen = virminen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "virhonen" } });
+  expect(inputName1.value).toBe("virhonen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "mäminen" } });
+  expect(inputName2.value).toBe("mäminen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("virminen")).toBeInTheDocument()
 
 });
 
-test('virhonen + mäminen = mähonen', () => {
+test('mäminen + virhonen = mähonen', () => {
   const { getByTestId } = render(<App />);
   const inputName1 = screen.getByTestId("name1");
-  fireEvent.change(inputName1, { target: { value: "korhonen" } });
-  expect(inputName1.value).toBe("korhonen");
+  fireEvent.change(inputName1, { target: { value: "mäminen" } });
+  expect(inputName1.value).toBe("mäminen");
 
   const inputName2 = screen.getByTestId("name2");
-  fireEvent.change(inputName2, { target: { value: "virtanen" } });
-  expect(inputName2.value).toBe("virtanen");
+  fireEvent.change(inputName2, { target: { value: "virhonen" } });
+  expect(inputName2.value).toBe("virhonen");
 
   const { getByText } = within(getByTestId("combination"));
-  expect(getByText("kortanen")).toBeInTheDocument()
+  expect(getByText("mähonen")).toBeInTheDocument()
+
+});
+
+test('string out of bounds error check for first rule', () => {
+  const { getByTestId } = render(<App />);
+  const inputName1 = screen.getByTestId("name1");
+  fireEvent.change(inputName1, { target: { value: "aa" } });
+  expect(inputName1.value).toBe("aa");
+
+  const inputName2 = screen.getByTestId("name2");
+  fireEvent.change(inputName2, { target: { value: "aa" } });
+  expect(inputName2.value).toBe("aa");
+
+  const { getByText } = within(getByTestId("combination"));
+  expect(getByText("aaaa")).toBeInTheDocument()
 
 });
